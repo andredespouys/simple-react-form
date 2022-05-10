dda39fc7467cf2a86854a25932746ad38a32b32a
Andre Despouys <andredespouys@gmail.com>
2022-05-10 10:03:46 +0200

    Update

---
+++ build/precache-manifest.11d731bacb8092ad61d05bd262f7dc88.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ebedd09dbcb324c29127cde96ca1d8",
+    "url": "/simple-react-form/index.html"
+  },
+  {
+    "revision": "f5a20ae52f3f586c5f8b",
+    "url": "/simple-react-form/static/js/2.ef9a33a5.chunk.js"
+  },
+  {
+    "revision": "3c1975e53b49208693ea",
+    "url": "/simple-react-form/static/js/main.268256d5.chunk.js"
+  },
+  {
+    "revision": "7609f0d925ee7d76aaee",
+    "url": "/simple-react-form/static/js/runtime-main.21a0ab14.js"
+  }
+]);
